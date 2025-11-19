c3e907b6c416a0a79abf7511cd6c2fb0bd1f5059
Jorge Mercado <135031824+JMercado555@users.noreply.github.com>
2025-11-19 19:58:09 -0300

    segunda version

---
+++ script.js
@@ -0,0 +1,20 @@
+$("#agregar").click(function(){
+    let texto = $("#tarea").val().trim();
+
+    if(texto === "") return;
+
+    $("#lista").append(
+        `<li class="list-group-item ajuste">
+            <span>${texto}</span>  <span class="badge text-bg-danger check-btn">X</span>
+         </li>
+        `
+    )
+
+    $("#tarea").val("");
+
+})
+
+
+$("#lista").on("click",".check-btn", function() {
+    $(this).closest("li").remove()
+})
